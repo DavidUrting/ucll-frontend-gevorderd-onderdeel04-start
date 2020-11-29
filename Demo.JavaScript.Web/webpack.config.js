137a4d70df581d0b1b8b66568de52f405683b40e
David Urting <david.urting@live.com>
2020-11-29 16:48:09 +0100

    Add project files.

---
+++ Demo.JavaScript.Web/webpack.config.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+    entry : {
+        main: "./src/index.js"
+    },
+    output : {
+        filename: "../wwwroot/js/basic-class-module-bundle.js"
+    },
+    devtool: "source-map"
+}
